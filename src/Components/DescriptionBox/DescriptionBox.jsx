60f7c6fc68db15574d13b2778f838f6b89a0b64a
Ankit Kumar Mohanta <ankitmohanta60@gmail.com>
2024-01-29 22:18:45 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,26 @@
+import './DescriptionBox.css'
+
+
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriprtionbox-nav-box">Description</div>
+            <div className="descriprtionbox-nav-box fade">Reviews(122)</div>
+        </div>
+        <div className="descriptionbox-description">
+             <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Quidem minima nesciunt accusantium nisi reprehenderit molestiae vel optio. 
+                Similique obcaecati, magnam consectetur soluta beatae voluptatum sint ipsa autem ullam alias numquam.
+             </p>
+             <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Saepe quae possimus itaque quas laudantium sequi ut. 
+                Quas quibusdam maiores, mollitia, dolorum exercitationem ab soluta in, 
+                ad aperiam repellendus id temporibus.
+             </p>
+        </div>
+    </div>
+  )
+}
